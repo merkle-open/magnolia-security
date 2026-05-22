7f3c7de787806d8ce192b227db16eb3fc7a7e370
eschleb <bjoern.eschle@merkle.com>
2026-05-22 12:26:41 +0200

    Revert "improve inMemoryUserManager performance"
    This reverts commit dcb3a9ee5befe2cfa67d7af069b3ed2a7f606e51.

--- magnolia-usermanager-inmemory/src/main/java/com/merkle/oss/magnolia/usermanager/inmemory/configuration/InstallInMemoryUserManagerTask.java
+++ magnolia-usermanager-inmemory/src/main/java/com/merkle/oss/magnolia/usermanager/inmemory/configuration/InstallInMemoryUserManagerTask.java
@@ -7,7 +7,6 @@ import info.magnolia.jcr.nodebuilder.Ops;
 import info.magnolia.jcr.nodebuilder.task.ErrorHandling;
 import info.magnolia.jcr.nodebuilder.task.NodeBuilderTask;
 import info.magnolia.jcr.util.NodeTypes;
-import info.magnolia.jcr.util.NodeUtil;
 import info.magnolia.jcr.util.PropertyUtil;
 import info.magnolia.module.InstallContext;
 import info.magnolia.module.delta.AbstractRepositoryTask;
@@ -35,7 +34,7 @@ public class InstallInMemoryUserManagerTask extends AbstractRepositoryTask {
     @Override
     protected void doExecute(final InstallContext ctx) throws TaskExecutionException {
         new NodeBuilderTask(getName(), getDescription(), ErrorHandling.strict, RepositoryConstants.CONFIG, PATH,
-                Ops.getOrAddNode(InMemoryUserManager.REALM.getName(), NodeTypes.ContentNode.NAME).then(orderFirst()).then(
+                Ops.getOrAddNode(InMemoryUserManager.REALM.getName(), NodeTypes.ContentNode.NAME).then(
                     setOrAddProperty("class", InMemoryUserManager.class.getName()),
                     setOrAddProperty("lockTimePeriod", 5), // in minutes
                     setOrAddProperty("maxFailedLoginAttempts", 3)
@@ -49,16 +48,6 @@ public class InstallInMemoryUserManagerTask extends AbstractRepositoryTask {
             protected Node doExec(final Node context, final ErrorHandler errorHandler) throws RepositoryException {
                 final Value value = PropertyUtil.createValue(newValue, context.getSession().getValueFactory());
                 context.setProperty(name, value);
-                return context;
-            }
-        };
-    }
-
-    public static NodeOperation orderFirst() {
-        return new AbstractNodeOperation() {
-            @Override
-            protected Node doExec(final Node context, final ErrorHandler errorHandler) throws RepositoryException {
-                NodeUtil.orderFirst(context);
                 return context;
             }
         };

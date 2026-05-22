dcb3a9ee5befe2cfa67d7af069b3ed2a7f606e51
eschleb <bjoern.eschle@merkle.com>
2026-05-22 10:00:35 +0200

    improve inMemoryUserManager performance
     - configure as first userManager to avoid info.magnolia.cms.security.MgnlUserManager.updatePreviousAccessTimestamp beeing triggered by delegatingUserManager

--- magnolia-usermanager-inmemory/src/main/java/com/merkle/oss/magnolia/usermanager/inmemory/configuration/InstallInMemoryUserManagerTask.java
+++ magnolia-usermanager-inmemory/src/main/java/com/merkle/oss/magnolia/usermanager/inmemory/configuration/InstallInMemoryUserManagerTask.java
@@ -7,6 +7,7 @@ import info.magnolia.jcr.nodebuilder.Ops;
 import info.magnolia.jcr.nodebuilder.task.ErrorHandling;
 import info.magnolia.jcr.nodebuilder.task.NodeBuilderTask;
 import info.magnolia.jcr.util.NodeTypes;
+import info.magnolia.jcr.util.NodeUtil;
 import info.magnolia.jcr.util.PropertyUtil;
 import info.magnolia.module.InstallContext;
 import info.magnolia.module.delta.AbstractRepositoryTask;
@@ -34,7 +35,7 @@ public class InstallInMemoryUserManagerTask extends AbstractRepositoryTask {
     @Override
     protected void doExecute(final InstallContext ctx) throws TaskExecutionException {
         new NodeBuilderTask(getName(), getDescription(), ErrorHandling.strict, RepositoryConstants.CONFIG, PATH,
-                Ops.getOrAddNode(InMemoryUserManager.REALM.getName(), NodeTypes.ContentNode.NAME).then(
+                Ops.getOrAddNode(InMemoryUserManager.REALM.getName(), NodeTypes.ContentNode.NAME).then(orderFirst()).then(
                     setOrAddProperty("class", InMemoryUserManager.class.getName()),
                     setOrAddProperty("lockTimePeriod", 5), // in minutes
                     setOrAddProperty("maxFailedLoginAttempts", 3)
@@ -48,6 +49,16 @@ public class InstallInMemoryUserManagerTask extends AbstractRepositoryTask {
             protected Node doExec(final Node context, final ErrorHandler errorHandler) throws RepositoryException {
                 final Value value = PropertyUtil.createValue(newValue, context.getSession().getValueFactory());
                 context.setProperty(name, value);
+                return context;
+            }
+        };
+    }
+
+    public static NodeOperation orderFirst() {
+        return new AbstractNodeOperation() {
+            @Override
+            protected Node doExec(final Node context, final ErrorHandler errorHandler) throws RepositoryException {
+                NodeUtil.orderFirst(context);
                 return context;
             }
         };
